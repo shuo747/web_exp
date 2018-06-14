c8013913dd9e2803fd5a45b9728e8ea489b59b31
shuo747 <228932747@qq.com>
2018-06-15 00:26:51 +0800

    第四次交实验了，就这样吧，好好期末复习吧，考个好成绩！！！

---
+++ src/dao/CustomerDAO.java
@@ -0,0 +1,35 @@
+package dao;
+
+import java.util.List;
+
+import domain.Customer;
+
+
+
+public interface CustomerDAO {
+	
+	/**
+	 * 返回满足查询条件的 List
+	 * @param cc: 封装了查询条件
+	 * @return
+	 */
+	public List<Customer> getForListWithCriteriaCustomer(CriteriaCustomer cc);
+	
+	public List<Customer> getAll();
+	
+	public void save(Customer customer);
+	
+	public Customer get(Integer id);
+	
+	public void delete(Integer id);
+	
+	public void update(Customer customer);
+	
+	/**
+	 * 返回和 name 相等的记录数. 
+	 * @param name
+	 * @return
+	 */
+	public long getCountWithName(String name);
+	
+}	
